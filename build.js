4bea073581ba95631f661056ff3f707bdd050878
Marco Labarile <18102614+labarilem@users.noreply.github.com>
2018-01-20 18:45:26 +0100

    added build script

---
+++ build.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var cpr = require('cpr');
+var fs = require('fs');
+
+// settings
+var distDirPath = 'dist';
+var toCopyInDistDir = [
+  'src/main.js',
+  'package.json',
+  'README.md'
+];
+
+// creting dist dir if it does not exist
+if (!fs.existsSync(distDirPath)) {
+  fs.mkdirSync(distDirPath);
+}
+
+// copying files to the dist dir
+toCopyInDistDir.forEach(fromPath => {
+  var toPath = path.join(distDirPath, path.basename(fromPath));
+  console.log('Copying file: \'' + fromPath + '\' to dist folder.');
+  cpr(fromPath, toPath, { overwrite: true }, (err, files) => {
+    if (!err) {
+      console.log('Successfully copied file: \'' + fromPath + '\' to dist folder.');
+    } else {
+      console.log('Error copying file: \'' + fromPath + '\' to dist folder.');
+    }
+  });
+});
